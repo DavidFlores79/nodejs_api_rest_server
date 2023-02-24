5d3d783bfa022966bafe7ef7009e131709e89b5d
David Flores <david.flores@itsoft.mx>
2023-02-23 18:28:43 -0600

    feat: :sparkles: Subir imagenes o archivos. Se extrajo a un Helper
    Subir imagenes o archivos. Se extrajo a un Helper

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,34 +1,26 @@
-const path = require('path')
+const { uploadFiles } = require('../helpers/uploads.helper');
 
+const uploadFile = async (req, res) => {
 
-const uploadFile = (req, res) => {
-
-  if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(404).send("No hay archivos para carga.")
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(404).send("No hay archivo para carga.")
     return
   }
-
-  if (!req.files.archivo) {
-    res.status(404).send("No hay archivo para subir.");
-    return;
+  console.log("req.files >>>", req.files); // eslint-disable-line
+  
+  try {
+    //   const { nombre, fullPath } = await uploadFiles(req.files, 'documents', ['txt', 'csv']);
+      const { nombre, fullPath } = await uploadFiles(req.files, 'users');
+    
+      res.status(201).send({
+        message: `Archivo guardado: ${nombre}`
+      })    
+  } catch (error) {
+    res.status(400).send({
+        message: error
+      })
   }
 
-  console.log("req.files >>>", req.files); // eslint-disable-line
-
-  const { archivo } = req.files;
-
-  const uploadPath =  path.join(__dirname, '../uploads', archivo.name);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-
-    res.send({ 
-        message: 'File uploaded to ' + uploadPath 
-    })
-
-  });
 };
 
 module.exports = { uploadFile };
